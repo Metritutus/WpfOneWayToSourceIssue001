d9cacff206189375f56c8ee128b2c34d2eb3c536
Metritutus <flying_saucers@hotmail.co.uk>
2021-07-28 21:41:30 +0100

    Updated UIElementBehaviors.cs to add workaround

--- Behaviors/UIElementBehaviors.cs
+++ Behaviors/UIElementBehaviors.cs
@@ -17,6 +17,41 @@ namespace WpfOneWayToSourceIssue001.Behaviors
             SetCurrentValue(ExampleBehaviorPropertyProperty, "Example Behavior Dependency Property");
             var value = GetValue(ExampleBehaviorPropertyProperty);
             System.Diagnostics.Debug.WriteLine($"Set value: {(value != null ? $"\"{value}\"" : "null")}");
+            
+            (AssociatedObject as FrameworkElement).DataContextChanged += UIElementBehaviors_DataContextChanged;
+        }
+        
+        private void UIElementBehaviors_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            var oldDataContext = e.OldValue as INotifyPropertyChanged;
+            var newDataContext = e.NewValue as INotifyPropertyChanged;
+
+            if (oldDataContext != null)
+            {
+                oldDataContext.PropertyChanged -= DataContext_PropertyChanged;
+            }
+
+            if (newDataContext != null)
+            {
+                newDataContext.PropertyChanged += DataContext_PropertyChanged;
+            }
+        }
+        
+        private void DataContext_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            var expression = BindingOperations.GetBindingExpression(this, ExampleBehaviorPropertyProperty);
+
+            if (sender != null && (e.PropertyName == null || e.PropertyName == expression.ResolvedSourcePropertyName))
+            {
+                var currentSourceValue = sender.GetType().GetProperty(e.PropertyName).GetValue(sender);
+
+                var dependencyPropertyValue = GetValue(ExampleBehaviorPropertyProperty);
+
+                if (dependencyPropertyValue != currentSourceValue)
+                {
+                    typeof(BindingExpressionBase).GetProperty("Value", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic).SetValue(expression, dependencyPropertyValue);
+                }
+            }
         }
     }
 }
